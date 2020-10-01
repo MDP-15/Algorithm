ab6677dc9278ca80abae335296780d0f1ad1e010
estherc <estherc2499@gmail.com>
2020-10-01 22:42:22 +0800

    Created buttons and text for user inputs

--- MDPsim/src/mdpsimGUI/ControlPanel.java
+++ MDPsim/src/mdpsimGUI/ControlPanel.java
@@ -15,8 +15,11 @@ import mdpMapReader.MapReader;
 public class ControlPanel extends JPanel{
 	Viewer vw;
 	JComboBox speedindicator;
+	JComboBox explorePercent;
 	JTextArea mdfinput;
+	JTextArea userTime;
 	JButton resetbutton;
+	JButton setTime;
 	
 	private static final long serialVersionUID = 1L;
 	public ControlPanel () {
@@ -31,6 +34,8 @@ public class ControlPanel extends JPanel{
 	}
 	
 	public void constructor() {
+		
+		//SELECT ROBOT SPEED
 		String[] strings = {"MAX","1x","0.5x","0.25x"};
 		this.speedindicator = new JComboBox(strings);
 		speedindicator.setBounds(5,5,100,20);
@@ -43,6 +48,37 @@ public class ControlPanel extends JPanel{
 			}
 		});
 		this.add(speedindicator);
+		
+		//SELECT % OF EXPLORATION
+		String[] explore = {"100%","75%","50%"};
+		this.explorePercent = new JComboBox(explore);
+		explorePercent.setBounds(110, 5, 100, 20);
+		explorePercent.setSelectedIndex(1);
+		explorePercent.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			//ADD ACTION
+			}
+		});
+		this.add(explorePercent);
+		
+
+		//USER SELECTED TIME LIMIT
+		this.userTime = new JTextArea("Input Time Limit in seconds");
+		userTime.setBounds(5,70,300,30);
+		this.add(userTime);
+		this.setTime = new JButton();
+		setTime.setText("Set Time");
+		setTime.setBounds(305,70, 100,30);
+		this.add(setTime);
+		setTime.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+			//ADD ACTION
+			}
+		});
+		
+		//INPUT MDF STRING
 		this.mdfinput = new JTextArea("Input an MDF string");
 		mdfinput.setBounds(5,35,300,30);
 		this.add(mdfinput);

c7f4c8fc61e7fed48c555c1d11c1f3ab8a3f439d
estherc <estherc2499@gmail.com>
2020-10-03 13:50:42 +0800

    added texts and buttons

--- MDPsim/src/mdpsimGUI/ControlPanel.java
+++ MDPsim/src/mdpsimGUI/ControlPanel.java
@@ -15,7 +15,7 @@ import mdpsim.MDPSIM;
 import mdpsimRobot.LogicHandler;
 import mdpsimRobot.Robot;
 
-public class ControlPanel extends JPanel{
+public class ControlPanel extends JPanel {
 	Viewer vw;
 	JComboBox speedindicator;
 	JComboBox explorePercent;
@@ -24,64 +24,96 @@ public class ControlPanel extends JPanel{
 	JButton resetbutton;
 	JButton flag;
 	JButton setTime;
-	
+
 	private static final long serialVersionUID = 1L;
-	public ControlPanel () {
+
+	public ControlPanel() {
 		super();
 		constructor();
 	}
-	
+
 	public ControlPanel(LayoutManager lm, Viewer vw) {
 		super(lm);
 		constructor();
 		this.vw = vw;
 	}
-	
+
 	public void constructor() {
-		
-		//SELECT ROBOT SPEED
-		String[] strings = {"MAX","1x","0.5x","0.25x"};
+
+		// SELECT ROBOT SPEED
+		String[] strings = { "MAX", "1x", "0.5x", "0.25x" };
 		this.speedindicator = new JComboBox(strings);
-		speedindicator.setBounds(5,5,100,20);
+		speedindicator.setBounds(5, 5, 100, 20);
 		speedindicator.setSelectedIndex(1);
 		speedindicator.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			vw.enginespeedflag = true;
-			vw.enginespeed = speedindicator.getSelectedIndex();
+				vw.enginespeedflag = true;
+				vw.enginespeed = speedindicator.getSelectedIndex();
 			}
 		});
 		this.add(speedindicator);
-		this.mdfinput = new JTextArea();
-		mdfinput.setBounds(5,25,300,20);
+
+		// SELECT % OF EXPLORATION
+		String[] explore = { "100%", "75%", "50%" };
+		this.explorePercent = new JComboBox(explore);
+		explorePercent.setBounds(110, 5, 100, 20);
+		explorePercent.setSelectedIndex(1);
+		explorePercent.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// ADD ACTION
+			}
+		});
+		this.add(explorePercent);
+
+		// USER SELECTED TIME LIMIT
+		this.userTime = new JTextArea("Input Time Limit in seconds");
+		userTime.setBounds(5, 70, 300, 30);
+		this.add(userTime);
+		this.setTime = new JButton();
+		setTime.setText("Set Time");
+		setTime.setBounds(305, 70, 100, 30);
+		this.add(setTime);
+		setTime.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// ADD ACTION
+			}
+		});
+
+		// INPUT MDF STRING
+		this.mdfinput = new JTextArea("Input an MDF string");
+		mdfinput.setBounds(5, 35, 300, 30);
 		this.add(mdfinput);
 		this.resetbutton = new JButton();
-		resetbutton.setText("Reset!");
-		resetbutton.setBounds(305,25,100,20);
+		resetbutton.setText("Enter!");
+		resetbutton.setBounds(305, 35, 100, 30);
 		this.add(resetbutton);
 		resetbutton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-			vw.custommdfresetflag = true;
-			vw.mdfstring = mdfinput.getText();
+				vw.custommdfresetflag = true;
+				vw.mdfstring = mdfinput.getText();
 			}
 		});
-		//FLAG BUTTON
-				flag = new JButton("Explore");
-				flag.setBounds(5, 120, 100, 30);
-				flag.addActionListener(new ActionListener() {
-				@Override
-				public void actionPerformed(ActionEvent e) {
-						if (MDPSIM.mode == 1) {
-							MDPSIM.mode = 0;
-						} else if (MDPSIM.mode == 0) {
-							MDPSIM.mode = 1;
-						}
-					}
-				});
-				this.add(flag);
+
+		// FLAG BUTTON
+		flag = new JButton("Explore");
+		flag.setBounds(5, 120, 100, 30);
+		flag.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				if (MDPSIM.mode == 1) {
+					MDPSIM.mode = 0;
+				} else if (MDPSIM.mode == 0) {
+					MDPSIM.mode = 1;
+				}
+			}
+		});
+		this.add(flag);
 	}
-	
+
 	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
